602c5ba820ca21d2e74cc4f64906bfe6a0958a9b
hhankj <77713686+hhankj@users.noreply.github.com>
2023-11-06 18:24:34 -0800

    Update experiment-no_fullscreen.js

--- experiment-no_fullscreen.js
+++ experiment-no_fullscreen.js
@@ -186,6 +186,30 @@ function CheckRestricted(src, restricted) {
 }
 
 /*************** TIMELINE ***************/
+var welcome = {
+  type: "html-keyboard-response",
+  stimulus: "Welcome to the experiment. Press any key to begin.",
+};
+timeline.push(welcome);
+var instructions = {
+  type: "html-keyboard-response",
+  stimulus: `
+    <p>In this experiment, a circle will appear in the center 
+    of the screen.</p><p>If the circle is <strong>blue</strong>, 
+    press the letter F on the keyboard as fast as you can.</p>
+    <p>If the circle is <strong>orange</strong>, press the letter J 
+    as fast as you can.</p>
+    <div style='width: 700px;'>
+    <div style='float: left;'><img src='img/blue.png'></img>
+    <p class='small'><strong>Press the F key</strong></p></div>
+    <div style='float: right;'><img src='img/orange.png'></img>
+    <p class='small'><strong>Press the J key</strong></p></div>
+    </div>
+    <p>Press any key to begin.</p>
+  `,
+  post_trial_gap: 2000,
+};
+timeline.push(instructions);
 for (let i = 1; i < 65; i++) {
   let targetCard = Object.values(cards).filter(
     (card) => card.trialNumber === i
