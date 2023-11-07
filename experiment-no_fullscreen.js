c13938e34257e4910e36c74020214d8d116cad0d
hhankj <77713686+hhankj@users.noreply.github.com>
2023-11-06 19:00:16 -0800

    Update experiment-no_fullscreen.js

--- experiment-no_fullscreen.js
+++ experiment-no_fullscreen.js
@@ -186,6 +186,31 @@ function CheckRestricted(src, restricted) {
 }
 
 /*************** TIMELINE ***************/
+/* define welcome message trial */
+var welcome_block = {
+    type: "html-keyboard-response",
+    stimulus: "Welcome to the experiment. Press any key to begin."
+};
+timeline.push(welcome_block);
+
+/* define instructions trial */
+var instructions = {
+    type: "html-keyboard-response",
+    stimulus: "<p>In this experiment, a circle will appear in the center " +
+        "of the screen.</p><p>If the circle is <strong>blue</strong>, " +
+        "press the letter F on the keyboard as fast as you can.</p>" +
+        "<p>If the circle is <strong>orange</strong>, press the letter J " +
+        "as fast as you can.</p>" +
+        "<div style='width: 700px;'>"+
+        "<div style='float: left;'><img src='img/blue.png'></img>" +
+        "<p class='small'><strong>Press the F key</strong></p></div>" +
+        "<div class='float: right;'><img src='img/orange.png'></img>" +
+        "<p class='small'><strong>Press the J key</strong></p></div>" +
+        "</div>"+
+        "<p>Press any key to begin.</p>",
+    post_trial_gap: 2000
+};
+
 var welcome = {
   type: "html-keyboard-response",
   stimulus: "Welcome to the experiment. Press any key to begin.",
@@ -316,6 +341,8 @@ jsPsych.init({
   },
 
   on_finish: function () {
+    Qualtrics.SurveyEngine.setEmbeddedData("success", accuracy);
+    Qualtrics.SurveyEngine.setEmbeddedData("browser_events", rt);
     jsPsych.data.get().localSave("csv", `WCST_subject_${subjectId}_output.csv`);
   },
 });
