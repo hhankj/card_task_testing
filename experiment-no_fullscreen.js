a3a819fac0f7e1ac4163eabe9494e26d7e4dadda
hhankj <77713686+hhankj@users.noreply.github.com>
2023-11-07 17:27:05 -0800

    Update experiment-no_fullscreen.js

--- experiment-no_fullscreen.js
+++ experiment-no_fullscreen.js
@@ -194,23 +194,7 @@ var welcome_block = {
 timeline.push(welcome_block);
 
 /* define instructions trial */
-var instructing = {
-    type: "html-keyboard-response",
-    stimulus: "<p>In this experiment, a circle will appear in the center " +
-        "of the screen.</p><p>If the circle is <strong>blue</strong>, " +
-        "press the letter F on the keyboard as fast as you can.</p>" +
-        "<p>If the circle is <strong>orange</strong>, press the letter J " +
-        "as fast as you can.</p>" +
-        "<div style='width: 700px;'>"+
-        "<div style='float: left;'><img src='img/blue.png'></img>" +
-        "<p class='small'><strong>Press the F key</strong></p></div>" +
-        "<div class='float: right;'><img src='img/orange.png'></img>" +
-        "<p class='small'><strong>Press the J key</strong></p></div>" +
-        "</div>"+
-        "<p>Press any key to begin.</p>",
-    post_trial_gap: 2000
-};
-timeline.push(instructing);
+timeline.push(instructions);
 
 for (let i = 1; i < 65; i++) {
   let targetCard = Object.values(cards).filter(
